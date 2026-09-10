905c09ce8b774a1c62e3fda88b9be7270321ed9d
niklastakla-byte <niklastakla@gmail.com>
2026-09-10 12:06:28 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// This is a generic conceptual example
+window.onload = function() {
+    // Attempts to open a new window immediately upon loading
+    window.open("https://example.com", "_blank", "width=800,height=600");
+};
