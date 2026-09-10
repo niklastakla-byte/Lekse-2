03d1b56624000beb56400a6c0de4037beb14bd01
niklastakla-byte <niklastakla@gmail.com>
2026-09-10 12:10:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // This is a generic conceptual example
 window.onload = function() {
-    // Attempts to open a new window immediately upon loading
-    window.open("https://example.com", "_blank", "width=800,height=600");
+    
+    window.open("https://google.com", "_blank", "width=800,height=600");
 };
